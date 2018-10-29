bc8fe64f566936a6bd5ecdd507beb59687d72edc
BikerGitHub <22326595+BikerGitHub@users.noreply.github.com>
2018-10-29 11:39:25 +0800

    数据库样例

---
+++ MySQL数据库基础/2018年10月29日/bookstore1.sql
@@ -0,0 +1,36 @@
+﻿drop database if exists bookstore;
+
+create database if not exists bookstore
+default character set utf8;
+use bookstore;
+
+CREATE TABLE  book (       
+ bookno  char(10)   NOT NULL,       
+ booktype  varchar(8)  NOT NULL  default 'computer',       
+ bookname       varchar(40)  NOT  NULL ,       
+ pubtime   date        NOT  NULL , 
+ publisher   varchar(40)   ,        
+ price      float(5,2)   ,       
+ num      int,
+ primary key(bookno)
+); 
+insert into book values('tp001','computer','how to learn java','2010-1-1','sz',100,1000);
+CREATE TABLE  members (       
+ userno  char(5)   NOT NULL,       
+ username  varchar(8)  NOT NULL,       
+ sex       char(2)  ,       
+ phone   varchar(8) ,       
+ regtime     date 
+); 
+
+CREATE TABLE  sell (       
+ orderno  char(5)   NOT NULL,       
+ userno  char(5)  NOT NULL ,       
+ bookno  char(10)   NOT NULL,       
+ num   int , 
+ price float,       
+ orderdate date  null,
+ ifsend int DEFAULT  0,
+ ifget int DEFAULT 0,
+ ifpay int DEFAULT  0
+); 
---
+++ MySQL数据库基础/2018年10月29日/yggl_teacher.sql
@@ -0,0 +1,26 @@
+﻿drop database if exists yggl;
+
+create database if not exists yggl;
+use yggl;
+
+CREATE TABLE  employees(
+ empno  varchar(6)  NOT  NULL primary key,
+ name   char(10)   NOT   NULL ,
+ education    char(4)   NOT NULL ,
+ birthdate  date        NOT NULL ,
+ sex    char(2)   NOT  NULL ,
+ workinglife   tinyint  NULL  ,
+ address  varchar(20)  NULL ,
+ telephone   char(12)  NULL ,
+ deptno  char(3)   NULL  
+);
+CREATE TABLE   departments  (
+ deptno char(3)   NOT NULL ,
+ deptname char(20)   NOT NULL  ,
+ remarks     text   NULL
+);
+CREATE TABLE   salary (
+ empno char(6)   NOT NULL ,
+ income float(8,2)   NOT NULL ,
+ expense float(8,2)  NOT NULL
+);
