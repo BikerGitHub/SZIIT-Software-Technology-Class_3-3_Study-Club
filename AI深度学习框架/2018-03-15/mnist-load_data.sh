7cd877fa83547557e0492b1d61ceaad54d412bbc
BikerGitHub <22326595+BikerGitHub@users.noreply.github.com>
2019-03-15 22:12:56 +0800

    Update mnist-load_data.sh

--- AI深度学习框架/2018-03-15/mnist-load_data.sh
+++ AI深度学习框架/2018-03-15/mnist-load_data.sh
@@ -1,2 +1,2 @@
 wget https://github.com/SZIIT-Software-Technology/SZIIT-Software-Technology-Class_3-3_Study-Club/raw/master/AI%E6%B7%B1%E5%BA%A6%E5%AD%A6%E4%B9%A0%E6%A1%86%E6%9E%B6/2018-03-15/mnist.npz
-mv -f mnist.npz '/home/biker/.keras/datasets/mnist.npz'
+mv -f mnist.npz '~/.keras/datasets/mnist.npz'
